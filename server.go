a2ba652173b32a723efd975962450f38e8410d39
a-wing <1@233.email>
2021-08-11 10:04:52 +0800

    Update server comment

--- server.go
+++ server.go
@@ -17,8 +17,9 @@ const (
 	readyStateClosed
 )
 
-// Core Websocket Server. use callback notification message
+// Server is lightcable core Server. use callback notification message
 // broadcast message, A Server auto create and manage multiple goroutines
+// every room create worker
 type Server struct {
 	config *Config
 	worker map[string]*worker
@@ -63,7 +64,8 @@ func New(cfg *Config) *Server {
 	}
 }
 
-// Need use 'go server.Run(context.Background())' run daemon
+// Run need use 'go server.Run(context.Background())' run daemon
+// in order to concurrency. server instance only a run
 func (s *Server) Run(ctx context.Context) {
 	s.readyState = readyStateRunning
 	for {
@@ -97,7 +99,8 @@ func (s *Server) Run(ctx context.Context) {
 	}
 }
 
-// Interface 'http.Handler', creates new websocket connection
+// ServeHTTP Interface 'http.Handler'.
+// creates new websocket connection
 // Maybe Create new Worker. worker == room
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if room, name, ok := s.onConnected(w, r); ok {
@@ -121,8 +124,10 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Broadcast will room all websocket connection send message
 // https://www.rfc-editor.org/rfc/rfc6455.html#section-11.8
 // code is websocket Opcode
+// name is custom name, this will be callback OnMessage
 func (s *Server) Broadcast(room, name string, code int, data []byte) {
 	s.broadcast <- Message{
 		Name: name,
@@ -132,36 +137,37 @@ func (s *Server) Broadcast(room, name string, code int, data []byte) {
 	}
 }
 
-// All Websocket Conn Recv Message will callback this function
+// OnMessage will all Websocket Conn Recv Message will callback this function
 // This have Block worker. Block this room
 func (s *Server) OnMessage(fn func(*Message)) {
 	s.onMessage = fn
 }
 
-// Auth this websocket connection callback
+// OnConnected auth this websocket connection callback
 // ok: true Allows connection; false Reject connection
 // Maybe Concurrent. unique ID need self use sync.Mutex
 func (s *Server) OnConnected(fn func(w http.ResponseWriter, r *http.Request) (room, name string, ok bool)) {
 	s.onConnected = fn
 }
 
-// websocket connection successfully. block worker
+// OnConnReady websocket connection successfully and join room
+// this will block worker
 func (s *Server) OnConnReady(fn func(*Client)) {
 	s.onConnReady = fn
 }
 
-// server safely shutdown done callback
+// OnServClose server safely shutdown done callback
 func (s *Server) OnServClose(fn func()) {
 	s.onServClose = fn
 }
 
-// This Worker All Conn all closed, worker close
+// OnRoomClose worker all websocket connection closed, worker close
 func (s *Server) OnRoomClose(fn func(room string)) {
 	s.onRoomClose = fn
 }
 
-// Client and err
-// if context server close err == nil
+// OnConnClose will Client error or websocket close or server close
+// if context server closed err == nil
 func (s *Server) OnConnClose(fn func(*Client, error)) {
 	s.onConnClose = fn
 }
