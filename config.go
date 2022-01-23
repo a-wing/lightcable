ba3bf955ebe2d4e43198c2edb9c9d9c5a2a02f3e
a-wing <1@233.email>
2022-01-24 00:40:44 +0800

    Add Server Broadcast local && nolocal

--- config.go
+++ config.go
@@ -6,6 +6,11 @@ type Config struct {
 	SignBufferCount int
 	CastBufferCount int
 
+	// If you set this option as `false`
+	// The server will not broadcast to you messages you send.
+	// Look like MQTTv5 nolocal
+	Local bool
+
 	Worker *Config
 }
 
@@ -13,6 +18,7 @@ type Config struct {
 var DefaultConfig = &Config{
 	SignBufferCount: 128,
 	CastBufferCount: 128,
+	Local:           false,
 
 	Worker: &Config{
 		SignBufferCount: 128,
